10eb6e6bce0310944f468e78ce6ad1f14579f0e0
Laith Nurie <laithnurie@gmail.com>
2015-08-23 13:29:22 +0100

    drawer layout setup

--- mobile/src/main/java/com/laithlab/rhythm/PlayerActivity.java
+++ mobile/src/main/java/com/laithlab/rhythm/PlayerActivity.java
@@ -1,7 +1,9 @@
 package com.laithlab.rhythm;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +13,18 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            actionBar.setHomeAsUpIndicator(android.R.drawable.dra);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
     }
 
     @Override
