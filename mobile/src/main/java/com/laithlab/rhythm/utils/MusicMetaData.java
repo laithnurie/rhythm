6328c00fa462fe8bb9c1c6b9315834269ba71e4a
Laith Nurie <laith.nurie@bskyb.com>
2016-02-23 22:58:25 +0000

    metadata crash

--- mobile/src/main/java/com/laithlab/rhythm/utils/MusicMetaData.java
+++ mobile/src/main/java/com/laithlab/rhythm/utils/MusicMetaData.java
@@ -5,32 +5,57 @@ import android.media.MediaMetadataRetriever;
 
 public class MusicMetaData {
 
-    private MediaMetadataRetriever metaRetriver;
+    private String artist;
+    private String album;
+    private String title;
+    private byte[] albumArt;
+    private Long duration;
 
     public MusicMetaData(String filePath) {
-        metaRetriver = new MediaMetadataRetriever();
-        metaRetriver.setDataSource(filePath);
+        MediaMetadataRetriever metaRetriver = new MediaMetadataRetriever();
+
+        try {
+            metaRetriver.setDataSource(filePath);
+
+            this.artist = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+            this.album = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+            this.title = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+            this.albumArt = metaRetriver.getEmbeddedPicture();
+            try {
+                this.duration = Long.valueOf(metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            } catch (Exception e) {
+                this.duration = 0L;
+            }
+            metaRetriver.release();
+        } catch (Exception e) {
+            this.artist = "Unknown Artist";
+            this.album = "Unknown Album";
+            this.title = filePath;
+            this.albumArt = null;
+            this.duration = 0L;
+            metaRetriver.release();
+        }
+
     }
 
     public String getSongTitle() {
-        return metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        return title;
     }
 
     public long getDuration() {
-        String durationStr = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
-        return Long.valueOf(durationStr);
+        return duration;
     }
 
     public byte[] getAlbumArt() {
-        return metaRetriver.getEmbeddedPicture();
+        return albumArt;
     }
 
     public String getArtistName() {
-        return metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+        return artist;
     }
 
     public String getAlbumName() {
-        return metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+        return album;
     }
 
 }
